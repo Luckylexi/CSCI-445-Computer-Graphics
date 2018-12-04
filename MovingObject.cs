1d36c8d146bd2d5bd97e8c47fb6f9d7fcee59a5b
Kellie Larsen <kdlarsen@mavs.coloradomesa.edu>
2018-12-04 15:22:14 -0700

    Unity code

---
+++ MovingObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour
+{
+	public float speed;
+	public Vector3 point;
+
+	void Start()
+	{
+		speed = 20f;
+	}
+
+	void Update()
+	{
+		transform.Translate(speed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed*Input.GetAxis("Vertical") * Time.deltaTime);
+		point = transform.position;
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.RotateAround(point, Vector3.up, 20 * Time.deltaTime);
+		}
+		else if (Input.GetKey(KeyCode.A))
+			transform.RotateAround(point, -Vector3.up, 20 * Time.deltaTime);
+	}
+}
